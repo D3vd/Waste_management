666d3fad573875e663aa5582356310f3621b87bc
Dev Daksan <dev.91096@gmail.com>
2020-05-03 17:53:44 +0530

    Added Update bin Level

--- backend/controller/level.js
+++ backend/controller/level.js
@@ -1,4 +1,22 @@
 let Bin = require('../models/bins');
+let Stats = require('../models/stats');
+
+async function update(id, level) {
+  let affected = 0;
+
+  await Bin.updateOne({ id }, { level }, (err, a) => (affected = a.n));
+
+  if (affected === 0) return false;
+
+  let statJSON = {
+    id,
+    level,
+  };
+
+  let statDoc = new Stats(statJSON);
+  await statDoc.save();
+  return true;
+}
 
 async function read(id) {
   let { level } = await Bin.findOne({ id });
@@ -6,4 +24,4 @@ async function read(id) {
   return level;
 }
 
-module.exports = { read };
+module.exports = { read, update };
--- backend/routes/level.js
+++ backend/routes/level.js
@@ -3,6 +3,25 @@ const router = express.Router();
 
 const Level = require('../controller/level');
 
+router.post('/', async (req, res) => {
+  const { id, level } = req.body;
+
+  let ok = await Level.update(id, level);
+
+  if (!ok) {
+    res.status(400).json({
+      code: 400,
+      message: `Bin no: ${id} does not exist`,
+    });
+    return;
+  }
+
+  res.status(200).json({
+    code: 200,
+    message: `Updated level for Bin No: ${id}`,
+  });
+});
+
 router.get('/:id', async (req, res) => {
   const id = req.params.id;
 
